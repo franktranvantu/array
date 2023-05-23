1e004971e2f735a1a3045adf7e4c3bbb41817c02
frank <franktranvantu@gmail.com>
2023-05-24 01:09:24 +0700

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,46 @@
+public class Array {
+  private int[] items;
+  private int count;
+
+  public Array(int length) {
+    items = new int[length];
+  }
+
+  public void insert(int item) {
+    if (count == items.length) {
+      int[] newItems = new int[count * 2];
+      for (int i = 0; i < count; i++) {
+        newItems[i] = items[i];
+      }
+      items = newItems;
+    }
+    items[count++] = item;
+  }
+
+  public void removeAt(int index) {
+    if (index < 0 || index >= count) {
+      return;
+    }
+    if (count >= 0) {
+      for (int i = index; i < count; i++) {
+        items[i] = items[i+1];
+      }
+      count--;
+    }
+  }
+
+  public int indexOf(int item) {
+    for (int i = 0; i < items.length; i++) {
+      if (item == items[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public void print() {
+    for (int i = 0; i < count; i++) {
+      System.out.println(items[i]);
+    }
+  }
+}
